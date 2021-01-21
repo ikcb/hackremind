8e5886e7a3b6e9f5e6f74d0206db24360e24f0a4
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-01-21 14:41:41 +0530

    feat: set up mongoose connection

--- app.js
+++ app.js
@@ -1,37 +1,57 @@
-const express = require('express');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const cors = require('cors');
+const express = require('express');
 const helmet = require('helmet');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 const { StatusCodes } = require('http-status-codes');
 require('dotenv').config();
 
 const BaseRouter = require('./routes/api');
 
-const app = express();
+const DEBUG = process.env.NODE_ENV !== 'production';
 
-/** **********************************************************************************
- *                              Set basic express settings
- ********************************************************************************** */
+/**
+ * Connect to MongoDB instance
+ */
+
+mongoose.connect(process.env.MONGO_URI, {
+  user: process.env.MONGO_USER,
+  pass: process.env.MONGO_PWD,
+  dbName: 'hackremind',
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => DEBUG && console.log("we're connected!"));
+
+/**
+ * Set basic express settings
+ */
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Enable CORS
+app.use(cors());
+
 // Show routes called in console during development
-if (process.env.NODE_ENV === 'development') {
-  app.use(logger('dev'));
-}
+DEBUG && app.use(logger('dev'));
 
 // Security
-if (process.env.NODE_ENV === 'production') {
-  app.use(helmet());
-}
+DEBUG || app.use(helmet());
 
 // Add APIs
 app.use('/api', BaseRouter);
 
 // Print API errors
-app.use((err, _req, res) => {
+app.use((err, req, res) => {
   console.error(err);
   return res.status(StatusCodes.BAD_REQUEST).json({
     error: err.message
