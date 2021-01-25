47bdfc44aefddcac0b9fd975e85409203e5f9f25
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-01-25 20:30:25 +0530

    feat: scrape data from devpost

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,19 @@
-const express = require('express');
-
-const router = express.Router();
+const fetch = require('node-fetch');
+const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
   res.json({ foo: 'bar' });
+});
+
+router.get('/test', async (req, res, next) => {
+  const response = await fetch(
+    `https://devpost.com/hackathons?challenge_type=all&page=${1}&search=&sort_by=Prize+Amount&utf8=%E2%9C%93`
+  );
+
+  const body = await response.text();
+
+  console.log(body);
+  res.json({ foo: 'bar', body });
 });
 
 module.exports = router;
