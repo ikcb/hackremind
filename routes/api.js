c3bff4d0b0a85e45f9fdf940258cf2acf46f57d1
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-01-25 20:33:26 +0530

    fix: remove test route from API

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,5 @@
-const fetch = require('node-fetch');
 const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
   res.json({ foo: 'bar' });
 });
-
-router.get('/test', async (req, res, next) => {
-  const response = await fetch(
-    `https://devpost.com/hackathons?challenge_type=all&page=${1}&search=&sort_by=Prize+Amount&utf8=%E2%9C%93`
-  );
-
-  const body = await response.text();
-
-  console.log(body);
-  res.json({ foo: 'bar', body });
-});
-
-module.exports = router;
