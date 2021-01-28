750235983c1186adacf1653ce242e3c2f5d320f7
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-01-28 17:28:02 +0530

    fix: use got instead of node-fetch

---
+++ utils/embed-generator.js
@@ -0,0 +1,33 @@
+const allowedHosts = [
+  'adventofcode.com',
+  'atcoder.jp',
+  'azspcs.net',
+  'bubblecup.org',
+  'codechef.com',
+  'codeforces.com',
+  'codeforces.com/gyms',
+  'codility.com',
+  'codingame.com',
+  'codingcompetitions.withgoogle.com',
+  'ctftime.org',
+  'devfolio.co',
+  'devpost.com',
+  'dmoj.ca',
+  'e-olymp.com',
+  'facebook.com/hackercup',
+  'hackerearth.com',
+  'hackerrank.com',
+  'icfpcontest.org',
+  'kaggle.com',
+  'leetcode.com',
+  'open.kattis.com',
+  'projecteuler.net',
+  'russianaicup.ru',
+  'spoj.com',
+  'tlx.toki.id',
+  'topcoder.com'
+];
+
+module.exports = {
+  allowedHosts
+};
