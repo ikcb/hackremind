c3ef6de0f89b8ca2454e01903f9b11df24d842c8
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-03-29 18:42:06 +0530

    fix: add fallback one pixel image

--- Notifier/generators/image.generator.js
+++ Notifier/generators/image.generator.js
@@ -1,7 +1,11 @@
 const probe = require('probe-image-size');
 
 // fixes image url to make sure that each embed has the same width
-module.exports = async (img, defaultURL = process.env.ONE_PX_IMG) => {
+module.exports = async (
+  img,
+  defaultURL = process.env.ONE_PX_IMG ||
+    'https://github.com/iiitkota-codebase/hackremind/raw/main/assets/520x1-00000000.png'
+) => {
   let url = defaultURL;
 
   // use img if it's not a placeholder, its A.R. >= 4:3 and width >= 432px
