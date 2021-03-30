c30c81e6ec52ca73e47537a052c7e9ecec320ee3
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-03-30 19:25:48 +0530

    fix: shortcircuit does not ignore exceptions

--- Notifier/generators/host.generator.js
+++ Notifier/generators/host.generator.js
@@ -2,5 +2,9 @@ const got = require('got');
 
 module.exports = async host => {
   // check if the host supports https
-  return (await got(`https://${host}`)).url || `http://${host}`;
+  try {
+    return (await got(`https://${host}`)).url;
+  } catch {
+    return `http://${host}`;
+  }
 };
--- Notifier/tuners/config.tuner.js
+++ Notifier/tuners/config.tuner.js
@@ -12,11 +12,11 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   CLIST_BEARER: process.env.CLIST_BEARER,
   WEBHOOK_URL: process.env.WEBHOOK_URL,
-  ICONS_URL:
-    `${process.env.ICONS_URL}${
-      process.env.ICONS_URL.endsWith('/') ? '' : '/'
-    }` ||
-    'https://raw.githubusercontent.com/iiitkota-codebase/hackremind/main/assets/icons/',
+  ICONS_URL: process.env.ICONS_URL
+    ? `${process.env.ICONS_URL}${
+        process.env.ICONS_URL.endsWith('/') ? '' : '/'
+      }`
+    : 'https://raw.githubusercontent.com/iiitkota-codebase/hackremind/main/assets/icons/',
   ONE_PX_IMG:
     process.env.ONE_PX_IMG ||
     'https://github.com/iiitkota-codebase/hackremind/raw/main/assets/520x1-00000000.png',
